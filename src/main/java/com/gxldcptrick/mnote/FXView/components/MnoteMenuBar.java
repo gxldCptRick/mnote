7b68664801e51bd69c261afe23f2acca28a847ce
Fermin Sandoval <ferminsandoval950@hotmail.com>
2019-02-22 13:35:25 -0700

    Deleted fxml files

--- src/main/java/com/gxldcptrick/mnote/FXView/components/MnoteMenuBar.java
+++
@@ -1,7 +0,0 @@
-package com.gxldcptrick.mnote.FXView.components;
-
-import javafx.scene.control.MenuBar;
-
-public class MnoteMenuBar extends MenuBar {
-
-}
--- src/main/java/com/gxldcptrick/mnote/Mnote.java
+++ src/main/java/com/gxldcptrick/mnote/Mnote.java
@@ -5,7 +5,17 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Mnote extends Application {
+
+    private final static double Screen_Width;
+    private final static double Screen_Height;
+
+    static {
+        Screen_Width = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+        Screen_Height = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+    }
 
     @Override
     public void start(Stage stage) {
